fc8066a902041fa6154e13219789f78fa00d664e
ASMABI <asmabicheriyath@gmail.com>
2024-03-26 05:30:40 +0000

    home_page

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -3,13 +3,27 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("WELCOME",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 35,color: Colors.deepPurple),),),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          "HOME PAGE",
+          style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile(
+                tileColor: (index + 1) % 5 == 0 ? Colors.deepPurpleAccent : Colors.lightBlue,
+              );
+            },
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: 50),
+      ),
     );
   }
-  
 }
